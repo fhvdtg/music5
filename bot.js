78f24d942e4459c9cfa3686081240077ef12e7c3
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-04-11 06:50:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "Y";
+const prefix = "5";
 /////////////////////////
 ////////////////////////
 
@@ -36,7 +36,7 @@ client.on('message', async msg =>{
     if(command === `ping`) {
     let embed = new Discord.RichEmbed()
     .setColor(3447003)
-    .setTitle("Pong!!")
+    .setTitle("5ping")
     .setDescription(`${client.ping} ms,`)
     .setFooter(`Requested by | ${msg.author.tag}`);
     msg.delete().catch(O_o=>{})
@@ -342,15 +342,34 @@ client.on('message', message => {
     }
 });
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ياسين قلك غني ,`,"http://twitch.tv/ Arab Server")
-client.user.setStatus("dnd")
-});
+
+client.on('message', message => {
+    var prefix = ".1";
+      if (!message.content.startsWith(prefix)) return;
+      var args = message.content.split(' ').slice(1);
+      var argresult = args.join(' ');
+      if (message.author.id == 410052613996937217) return;
+    
+    if (message.content.startsWith(prefix + 'p')) {
+      client.user.setGame(argresult);
+        message.channel.sendMessage(`**${argresult}** : Status changed`)
+    } else
+    
+    if (message.content.startsWith(prefix + 's')) {
+      client.user.setGame(argresult, "https://www.twitch.tv/ChampionBot");
+        message.channel.sendMessage(`**${argresult}** :The bot stream has been changed`)
+    } else
+    
+    if (message.content.startsWith(prefix + 'n')) {
+      client.user.setUsername(argresult).then
+          message.channel.sendMessage(`**${argresult}** : Name changed`)
+      return message.reply("**You**");
+    } else
+    if (message.content.startsWith(prefix + 'i')) {
+      client.user.setAvatar(argresult);
+        message.channel.sendMessage(`**${argresult}** : The bot image has been changed`);
+    
+    }
+    });
 
 client.login(process.env.BOT_TOKEN);
